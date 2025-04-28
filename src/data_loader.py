03cc8f481de0ac35eb6c22a6f1dc485397d6f708
Koji <kjk227@cornell.edu>
2025-04-27 22:22:22 -0400

    Dataloader kind of?

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,7 +3,6 @@ from torch.utils.data import Dataset
 from tokenizer import tokenize
 from utils import Policy, MOVE_TO_ACTION, ACTION_TO_MOVE
 from bagz import BagReader
-import struct
 from apache_beam import coders
 
 CODERS = {
@@ -30,16 +29,35 @@ def _process_move(move: str) -> torch.tensor:
 
 def _process_win_prob(
     win_prob: float,
-    return_buckets_edges: torch.tensor,
+    bins_edges: torch.tensor,
 ) -> torch.tensor:
-    pass
+    returns = torch.tensor([win_prob])
+    if returns.dim() != 1:
+        raise ValueError(f'Returns should be 1D, got {returns.dim()}D.')
+
+    if bins_edges.dim() != 1:
+        raise ValueError(f'Bin edges should be 1D, got {bins_edges.dim()}D.')
+
+    return torch.searchsorted(bins_edges, returns, right=False)
+
+
+def _get_uniform_buckets_edges_values(num_buckets: int) -> tuple[torch.Tensor, torch.Tensor]:
+    full_linspace = torch.linspace(0.0, 1.0, steps=num_buckets + 1)
+
+    edges = full_linspace[1:-1]
+    values = (full_linspace[:-1] + full_linspace[1:]) / 2
+
+    return edges, values
 
 
 class ChessDataset(Dataset):
-    def __init__(self, bag_path, policy='action_value', num_return_bucket=128):
+    def __init__(self, bag_path, policy=Policy.ACTION_VALUE, num_return_buckets=128):
         self.bag_reader = BagReader(bag_path)
         self.policy = policy
-        self.num_return_bucket = num_return_bucket
+        self.num_return_buckets = num_return_buckets
+        self._return_buckets_edges, _ = _get_uniform_buckets_edges_values(
+            num_return_buckets,
+        )
 
     def __len__(self):
         return len(self.bag_reader)
@@ -47,12 +65,12 @@ class ChessDataset(Dataset):
     def __getitem__(self, idx):
         record = self.bag_reader[idx]
 
-        if self.policy == 'action_value':
+        if self.policy == Policy.ACTION_VALUE:
             fen, move, win_prob = CODERS['action_value'].decode(record)
             state = _process_fen(fen)
             action = _process_move(move)
             return_bucket = _process_win_prob(
-                win_prob, self.return_buckets_edges)
+                win_prob, self._return_buckets_edges)
             sequence = torch.from_numpy(
                 torch.concatenate([state, action, return_bucket]))
         else:
